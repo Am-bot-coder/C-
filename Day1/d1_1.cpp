156bd4e33810322e353083a7bec31b574de0e26f
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-03 15:19:16 +0530

    1st complete

--- Day1/d1_1.cpp
+++ Day1/d1_1.cpp
@@ -1,11 +1,17 @@
 #include<stdio.h>
 
-// void print();
+void print(); //global declaration
 int main(void){
-    // void print();
+    // void print();    //local declaration
     print();
     return 0;
 }
 void print(void){
     printf("Hello World");
 }
+
+/* 
+g++ d1_1.cpp
+.\a.exe
+>>>Hello World 
+*/
