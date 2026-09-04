b6d54cdc79d6ba920fd817476297880dd8a70cbd
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-04 17:40:30 +0530

    how to print global variable

--- Day2/priority_local_vs_global.cpp
+++ Day2/priority_local_vs_global.cpp
@@ -7,4 +7,9 @@ int main(){
     int num1 = 20;
     cout<<"it prints ->"<<num1<<endl; //local variable gets precedance
 
+    //To print the global variable
+    cout<<"it prints ->"<<::num1<<endl;//global variables print
+
 }
+
+
