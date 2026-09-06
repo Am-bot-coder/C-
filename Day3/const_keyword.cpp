69b4103f71d1bd28a12f6379b0cd22019948d502
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-06 22:22:41 +0530

    notes added

--- Day3/const_keyword.cpp
+++ Day3/const_keyword.cpp
@@ -6,6 +6,7 @@ class Test{
     //for declare the const variable there are 2 methods.
     const int num; 
     const int num2 = 30; //directly 
+    mutable int a;
      
     public:
     Test(void):num(0)//We can declare const by here
@@ -14,6 +15,23 @@ class Test{
         this->num = 0 //We cant do that
         */
     }
+    void print() const{ //declare a const function
+    //but inside that  we cant change the any variable
+    //just use for print 
+    //not for changing the variables.
+
+    //if you still need to change the variable in constant member function
+    //you need keyword --> mutable;
+
+    //by declaring mutable keyword  we can edit it in te const function
+    }
 //Thats it only 2 methods
 //we cant directly access constant keyword 
 };
+
+//ctor cannot be const 
+//we cannot declare global function const , 
+//class cannot const 
+//local pointer and global pointer can be made const 
+//data-member can be const 
+//member funtion  can be const 
