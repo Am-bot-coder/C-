2003812ced3a7dbf5a9219204be08438eea82c43
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-07 17:45:19 +0530

    calloclearn

---
+++ Day4/dynamic_memory_calloc.cpp
@@ -0,0 +1,22 @@
+//calloc takes 2 parameter
+//unlike malloc it required 1.number of blocks, 2.size;
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    int *ptr = (int*)calloc(4,sizeof(int));
+    if(ptr!=NULL){
+        ptr[0]=100;
+        ptr[1]=200;
+
+    }
+    for(int i=0;i<4;i++){
+        cout<<"Values-->"<<ptr[i]<<endl;
+        }
+    return 0;
+}
+//calloc by default set velue = 0;
+//malloc gets garbage value
