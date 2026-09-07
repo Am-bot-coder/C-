04b8b0e02595968b3b3c625a82eaea8a396f83f3
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-07 22:45:08 +0530

    2D array using c++

---
+++ Day4/dynamic_2D_array_using cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int **ptr = new int*[4];
+    for(int index=0;index<4;index++){
+        ptr[index] = new int[5];
+    }
+    for(int index=0;index<4;index++){
+        delete[] ptr[index];
+        ptr[index] = NULL;
+    }
+    delete[] ptr;
+    ptr = NULL;
+
+    return 0;
+}
