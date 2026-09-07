1eba2dd516aebd98fe5eeef797fc8807cca7cba0
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-07 17:57:47 +0530

    realloc

--- Day4/dynamic_memory_realloc.cpp
+++ Day4/dynamic_memory_realloc.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main(){
+    //using malloc
+    
     int *ptr = (int*)malloc(3*sizeof(int));
     ptr[0]=100;
     ptr[1]=200;
@@ -19,5 +21,32 @@ int main(){
 
     free(ptr);
     ptr = NULL;
+
+
+
+    //using calloc
+    int *ptr = (int*)calloc(3,sizeof(int));
+    ptr[0]=100;
+    ptr[1]=200;
+    ptr[2]=300;
+
+
+    ptr = (int*)realloc(ptr,2*sizeof(int));
+    ptr[3]=400;
+    ptr[4]=500;
+
+    for(int index = 0;index<5;index++){
+        cout<<ptr[index]<<endl;
+    }
+
+    free(ptr);
+    ptr = NULL;
+
+//realloc is for reallocating memory
+//it two 2 things ->
+//either gave next 2 subsequent memory addresses;
+//else copy that array value in new array size of after allocation
+
     return 0;
 }
+
