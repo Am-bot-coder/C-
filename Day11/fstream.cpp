82969e8008d2eb403f670cf435515f2c7c8f5e00
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-15 15:46:14 +0530

    fstream

--- Day11/fstream.cpp
+++ Day11/fstream.cpp
@@ -7,19 +7,24 @@ using namespace std;
 int main()
 {
     fstream file;
-    file.open("sample.text");
+    file.open("sample.text",ios::in | ios::out | ios::trunc);
 
     if(file.is_open()){
         file<<"Hello Everybody"<<endl;
         file<<"nice to meet you"<<endl;
         file<<" my name is Ayush"<<endl;
-    }
 
-    char ch;
-    while(file.get(ch)){
-        cout<<ch;
+
+        char ch;
+        file.seekg(0);// start file from begining
+        while(file.get(ch)){
+            cout<<ch;
+                            }
+    
     }
     file.close();
+
+    
 
 
     return 0;
